98d849a084a8ee5376c286b6da414c6b97bee253
Al Sweigart <asweigart@gmail.com>
2016-10-05 14:38:58 -0700

    Finished rektangle program

--- src_used_in_twitchtv/rektangle.py
+++ src_used_in_twitchtv/rektangle.py
@@ -1,8 +1,27 @@
 # https://www.reddit.com/r/dailyprogrammer/comments/4tetif/20160718_challenge_276_easy_recktangles/
 import logging
+import doctest
 logging.basicConfig(level=logging.DEBUG)
+logging.disable(logging.CRITICAL)
 
 def getRow(word, width, startReversed):
+	"""
+	>>> getRow('REKT', 1, False)
+	'R E K T'
+	>>> getRow('REKT', 1, True)
+	'T K E R'
+	>>> getRow('REKT', 2, False)
+	'R E K T K E R'
+	>>> getRow('REKT', 2, True)
+	'T K E R E K T'
+	>>> getRow('REKT', 3, False)
+	'R E K T K E R E K T'
+	>>> getRow('REKT', 3, True)
+	'T K E R E K T K E R'
+	"""
+	#if width % 2 == 0:
+	#	startReversed = not startReversed
+
 	theRow = []
 	for i in range(width):
 		if not startReversed:
@@ -18,26 +37,65 @@ def getRow(word, width, startReversed):
 
 		logging.debug('%s %s' % (wordInRow, wordInRow[1:]))
 		logging.debug('Before: %s' % (theRow[index]))
-		theRow[index] = wordInRow[1:]
+		theRow[index] = wordInRow[2:]
 		logging.debug('After: %s' % (theRow[index]))
 
 	return ' '.join(theRow)
 
-def displayRektangle(word, width, height):
-	# print out row. (even rows are forwards, odd rows are backwards)
-	for row in range(height + 1):
-		if row % 2 == 0:
-			print(getRow())
+def getInBetweenRows(word, width, wordIndex):
+	'''
+	>>> getInBetweenRows('REKT', 1, 1)
+	'E     K'
+	>>> getInBetweenRows('REKT', 1, 2)
+	'K     E'
+	>>> getInBetweenRows('REKT', 2, 1)
+	'E     K     E'
+	>>> getInBetweenRows('REKT', 2, 2)
+	'K     E     K'
+	>>> getInBetweenRows('REKTANGLE', 1, 5)
+	'N               T'
+	'''
+	letter = word[wordIndex]
+	altLetter = word[-(wordIndex + 1)]
+
+	theRow = []
+	appendAlt = False
+	for i in range(width + 1):
+		if not appendAlt:
+			theRow.append(letter)
 		else:
-			print(getRow())
+			theRow.append(altLetter)
+		appendAlt = not appendAlt
+	spacesInBetweenEachLetter = (' ' * (((len(word) - 2) * 2) + 1))
+	return spacesInBetweenEachLetter.join(theRow)
 
-print(getRow('REKT', 1, False))
-print(getRow('REKT', 1, True))
-print(getRow('REKT', 2, False))
-print(getRow('REKT', 2, True))
-print(getRow('REKT', 3, False))
-print(getRow('REKT', 3, True))
 
+def displayRektangle(word, width, height):
+	if width % 2 == 0:
+		startReversed = True
+	else:
+		startReversed = False
+
+	# print out row. (even rows are forwards, odd rows are backwards)
+	for rowNumber in range(height):
+		# print the entire row from getRow()
+		print(getRow(word, width, startReversed))
+
+
+		# print the in between rows from getInBetweenRows()
+		for inBetweenRowNumber in range(1, len(word) - 1):
+			if startReversed:
+				print(getInBetweenRows(word, width, len(word) - inBetweenRowNumber))
+			else:
+				print(getInBetweenRows(word, width, inBetweenRowNumber))
+
+
+		startReversed = not startReversed
+
+	print(getRow(word, width, startReversed))
+
+
+doctest.testmod()
 
 displayRektangle('REKT', 1, 1)
 print()
