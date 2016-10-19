6725df2dd6a676e87903d14542f2b48b5c5c778b
Al Sweigart <asweigart@gmail.com>
2016-10-18 17:54:05 -0700

    Solved the run length encoding problem.

--- exercismdotio/python/run-length-encoding/run_length.py
+++ exercismdotio/python/run-length-encoding/run_length.py
@@ -1,5 +1,6 @@
 import doctest
 import logging
+import re
 logging.basicConfig(level=logging.DEBUG)
 
 
@@ -32,40 +33,35 @@ def splitNumbersAndLetters(theString):
 	>>> splitNumbersAndLetters('12A4B')
 	['12', 'A', '4', 'B']
 	>>> splitNumbersAndLetters('12A4BC')
-	['12', 'A', '4', 'B', '1', C']
+	['12', 'A', '4', 'B', '1', 'C']
 	>>> splitNumbersAndLetters('ABC')
 	['1', 'A', '1', 'B', '1', 'C']
 	"""
-	splitList = []
+	pat = re.compile(r'(\d+|\D)')
+	splitList = pat.findall(theString)
 
-	if not theString[0].isdigit():
-		mode = 'nondigits'
-	else:
-		mode = 'digits'
-
-	currentRunLength = 0
-	for index, character in enumerate(theString):
-		if (mode == 'nondigits' and character.isdigit()) or (mode == 'digits' and not character.isdigit()):
-			# there's been a change in mode
-			splitList.append(theString[index-currentRunLength:index])
-			if mode == 'nondigits':
-				mode = 'digits'
-			elif mode == 'digits':
-				mode = 'nondigits'
-			else:
-				assert False
-
-			currentRunLength = 1
-		else:
-			# there's been no change in mode
-			currentRunLength += 1
-
-	splitList.append(theString[index-currentRunLength+1:])
+	# insert any missing '1' strings
+	expectingNumber = True
+	index = 0
+	while index < len(splitList):
+		if expectingNumber and not splitList[index].isdigit():
+			splitList.insert(index, '1')
+		expectingNumber = not expectingNumber
+		index += 1
 	return splitList
 
 def decode(compressedStr):
+	"""
+	>>> decode('12A4B')
+	'AAAAAAAAAAAABBBB'
+	"""
+	splitList = splitNumbersAndLetters(compressedStr)
 
-	return
+	uncompressedStr = []
+	for index in range(0, len(splitList), 2):
+		uncompressedStr.append( int(splitList[index]) * splitList[index + 1] )
+	
+	return ''.join(uncompressedStr)
 
 
 doctest.testmod()
