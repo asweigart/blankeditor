5862cdacc23229e9625f78f3a4703d16aa8a58f7
Al Sweigart <asweigart@gmail.com>
2017-01-19 22:06:43 -0800

    Going through the Django tutorial (completed up to part 4)

--- TDDbook/superlists/lists/tests.py
+++ TDDbook/superlists/lists/tests.py
@@ -1,7 +1,9 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
 
-  def test_bad_maths(self):
-    self.assertEqual(1 + 1, 3)
-
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
--- djangoTut/mysite/mysite/urls.py
+++ djangoTut/mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""superlists URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-#from django.contrib import admin
-from lists import views as lists_views
+from django.conf.urls import include, url
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', lists_views.home_page, name='home'),
-    #url(r'^blog/', include('blog.urls')),
-    #url(r'^admin/', admin.site.urls),
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
